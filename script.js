c903e17df39791a55287ae084234b5476349ce76
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2022-02-20 19:58:05 +0330

    second step :)

--- script.js
+++ script.js
@@ -6,13 +6,29 @@ let listMovies=document.querySelector("#listMovies")
 
 const card=(data)=>{
     for(let movie of data){
+        let div=document.createElement("div")
+        div.classList.add("myImages")
         let p=document.createElement("p");
         p.classList.add("para")
         p.innerText=movie.name;
+        let p2=document.createElement("p")
+        p2.classList.add("Summery")
+        p2.innerText=movie.summary.replace("<p>", "").replace("</p>", "");
         let img=document.createElement("img");
         img.src=movie.image.medium;
-        container.append(img);
-        container.append(p);
+        let i=document.createElement("i")
+        i.classList.add("bi","bi-play","button")
+        let i2=document.createElement("i")
+        i2.classList.add("bi","bi-clock","button")
+        div.append(img);
+        div.append(p);
+        div.append(p2);
+        div.append(i);
+        div.append(i2);
+
+        container.append(div);
+        // container.append(p);
+
     }
 }
 
